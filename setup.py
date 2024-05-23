5059bca6f390ef09ecc7bf8ac4923099b41ff5f0
Ryan Moore <ryanm.inbox@gmail.com>
2024-05-22 22:08:00 -0400

    Add rmtree build dist .so to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+from pathlib import Path
+from shutil import rmtree
 from setuptools import setup, Extension
+
+for dir in ['build', 'dist']:
+    if Path(dir).exists():
+        rmtree(dir)
+
+for so in Path('.').glob('value.cpython*'):
+    Path(so).unlink()
 
 setup(
     name='value',
